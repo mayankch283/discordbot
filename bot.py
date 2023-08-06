2d87fb6b4f07844c47e3a034afa2a0fd2392c0fc
mayankch283 <mayankch283@gmail.com>
2023-08-06 16:10:14 +0530

    Spam Detection bot

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
-import os
-
 import discord
+from discord.ext import commands
+from datetime import timedelta
+
+class aclient(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        super().__init__(intents = intents)
+        self.anti_spam = commands.CooldownMapping.from_cooldown(5,15,commands.BucketType.member)
+        self.too_many_violations = commands.CooldownMapping.from_cooldown(4,60,commands.BucketType.member)
+
+
+    async def on_ready(self):
+        print("Logged in as {self.user}.")
+
+    async def on_message(self, message):
+        if type(message.channel) is not discord.TextChannel or message.author.bot: return
+        bucket = self.anti_spam.get_bucket(message)
+        retry_after = bucket.update_rate_limit()
+        if retry_after:
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}, don't spam!", delete_after = 10)
+            violations = self.too_many_violations.get_bucket(message)
+            check = violations.update_rate_limit()
+            if check:
+                await message.author.timeout(timedelta(minutes = 10), reason = "Spamming")
+                try: await message.author.send("You have been muted for spamming")
+                except : pass
+client = aclient()
+
+import os
 from dotenv import load_dotenv
-
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 client.run(TOKEN)
